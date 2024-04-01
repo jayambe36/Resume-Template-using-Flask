f1e1a7af2794dfe70157bbbd8fda93706f232ff9
jayambe36 <smitrpatel19@gnu.ac.in>
2024-04-02 01:39:26 +0530

    update a code for deploy using vercel

--- liveWebDev/Scripts/activate.bat
+++ liveWebDev/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=G:\all project\FLASK PROJECT LEARN\liveWebDev
+set VIRTUAL_ENV=G:\all project\FLASK PROJECT LEARN\ResumeUsingFlask\liveWebDev
 
 if not defined PROMPT set PROMPT=$P$G
 
